2378ae7c214048f42aa60d50c1eac7f4396d9e8a
v1p3er <cybertechik@protonmail.com>
2022-06-04 18:01:02 +0430

    setted up login and sign up

--- home/urls.py
+++ home/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="main"),
+    path('signup-login', views.signup_login, name="signup-login"),
 ]
--- home/views.py
+++ home/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'main/index.html')
+def signup_login(request):
+    return render(request, 'main/signup-login.html')
+
