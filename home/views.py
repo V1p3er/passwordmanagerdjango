a1cf3d75f46b6c880d820e0e04c40df4b0272ecf
v1p3er <cybertechik@protonmail.com>
2022-06-04 18:30:07 +0430

    created forms.py and configured views function but doesnt work and trying to fix it, also made superuser

--- home/views.py
+++ home/views.py
@@ -1,7 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import NewUserForm
+from django.contrib.auth import login
+from django.contrib import messages
 
 def home(request):
     return render(request, 'main/index.html')
-def signup_login(request):
-    return render(request, 'main/signup-login.html')
 
+def signup_login(request):
+	if request.method == "POST":
+		form = NewUserForm(request.POST)
+		if form.is_valid():
+			user = form.save()
+			login(request, user)
+			messages.success(request, "Registration successful." )
+			return redirect("main:homepage")
+		messages.error(request, "Unsuccessful registration. Invalid information.")
+	form = NewUserForm()
+	return render (request=request, template_name="main/signup-login.html", context={"signup_login":form})
