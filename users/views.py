03890f8f4c3f42ab5745557aa30a2dfdcd13cec9
v1p3er <cybertechik@protonmail.com>
2022-06-08 11:12:38 +0430

    made login and sign up template

--- users/views.py
+++ users/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import UserCreationForm
 
 
-def signup_login(response):
-	return render(response, 'users/users.html')
-	
+def signup(request):
+    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+    return render(request, 'users/signup.html', {'form': form})
+def login(request):
+    render(request, template_name='users/login.html')
