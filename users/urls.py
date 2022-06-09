a54314bbb526054983272936a80877404fe54830
v1p3er <cybertechik@protonmail.com>
2022-06-09 16:40:39 +0430

    setted up full working login page

--- users/urls.py
+++ users/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('signup/', views.signup, name="signup"),
-    path('login/', views.login, name="login"),
+    path('login/', views.login_v, name="login"),
+    path('dashboard/', views.dashboard, name="dashboard"),
 ]
--- users/views.py
+++ users/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from .forms import CreateUserForm
 from django.contrib import messages
 
@@ -17,10 +17,19 @@ def signup(request):
         form = CreateUserForm()
     return render(request, template_name=('users/signup.html'), context={'form': form})
 
-def login(request):
-    form = CreateUserForm()
+def login_v(request):
     if request.method == 'POST':
-        form = CreateUserForm()(request.POST)
-        if form.is_valid():
-            form.save()
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user.is_authenticated:
+            login(request, user)
+            messages.success(request, 'Logged in successfully!')
+            return redirect('dashboard')
+        else:
+            messages.error(request, 'Invalid username or password')
+            return redirect('login/')
     return render(request, template_name=('users/login.html'))
+
+def dashboard(request):
+    return render(request, template_name=('users/dashboard.html'))
